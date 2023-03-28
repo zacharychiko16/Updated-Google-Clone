811cba0a0e4b1381c6e35ac567622c7110eca629
zacharychiko16 <zacharychiko16@gmail.com>
2023-03-28 19:20:07 -0400

    added footer component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/Footer';
 import './global.css';
 
 export const metadata = {
@@ -8,7 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>{children}
+      <Footer/>
+      </body>
     </html>
   )
 }
---
+++ src/components/CountryLookup.jsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function CountryLookup() {
+  const [country, setCountry] = useState("United States");
+  useEffect(() => {
+    fetch(
+      `http://api.ipstack.com/access_key=${process.env.NEXT_PUBLIC_IP_API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => setCountry(data.country));
+  }, []);
+  return <div>{country}</div>;
+}
+
+// "use client"
+
+// import { useEffect, useState } from "react";
+
+// export default function CountryLookup() {
+//   const [country, setCountry] = useState("United States");
+
+//   useEffect(() => {
+//     fetch("https://api.ipify.org?format=json")
+//       .then((res) => res.json())
+//       .then((data) => {
+//         const ip = data.ip;
+//         fetch(
+//           `http://api.ipstack.com/${ip}?access_key=${process.env.NEXT_PUBLIC_IP_API_KEY}`
+//         )
+//           .then((res) => res.json())
+//           .then((data) => console.log(data))
+//       });
+//   }, []);
+
+//   return <div>{country}</div>;
+// }
+
+// import { useEffect, useState } from "react";
+
+// export default function CountryLookup() {
+//   const [country, setCountry] = useState("United States");
+
+//   useEffect(() => {
+//     fetch("http://api.ipstack.com/134.201.250.155?output=json")
+//       .then((res) => res.json())
+//       .then((data) => {
+//         const ip = data.ip;
+//         fetch(
+//           `http://api.ipstack.com/${ip}?access_key=52393034d77cabdbfbf400675954a6de`
+//         )
+//           .then((res) => res.json())
+//           .then((data) => setCountry(data.country_name));
+//       });
+//   }, []);
+
+//   return <div>{country}</div>;
+// }
+
