af3e3914b10fce1e90d422b0f0c2e25e52cd6661
zacharychiko16 <zacharychiko16@gmail.com>
2023-03-29 18:19:48 -0400

    created pages for web &image

---
+++ src/app/search/layout.jsx
@@ -0,0 +1,12 @@
+import SearchHeader from "@/components/SearchHeader";
+import "./../global.css";
+
+export default function SearchLayout({ children }) {
+  return (
+    <div>
+      <SearchHeader />
+      {children}
+      {/* Footer */}
+    </div>
+  );
+}
