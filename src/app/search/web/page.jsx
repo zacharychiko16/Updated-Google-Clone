0a6294935747e9f504bdcc23da46b10ce2d77237
zacharychiko16 <zacharychiko16@gmail.com>
2023-03-30 17:39:04 -0400

    handle possible errors

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-
-export default function WebSearchPage() {
+export default async function WebSearchPage({searchParams}) {
+  const response = await fetch (
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  )
   return (
     <div>WebSearchPage</div>
-  )
+  ) 
 }
